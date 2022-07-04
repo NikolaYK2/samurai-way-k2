3605fe1f14bce2f1756b2888b6de28993713ab93
k2 <izmail.k2@gmail.com>
2022-07-04 23:00:35 +0300

    social network Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import './App.css';
-import Tehnologies from "./components/Tehnologies/Tehnologies";
-import Header from "./components/Header/Header";
-import Accordion from "./components/Accordion/Accordion";
-import Rating from "./components/Rating/Rating";
 
 function App() {
     console.log("App rendering")
     return (
-        <div className="App">
-            <input/>
-            <input type="password"/>
-            <PageTitle title={'Hello, samurai! Let\'s go!'}/>
-            Article 1
-            <Rating value={1}/>
-            <Header/>
-            <Tehnologies/>
-            <PageTitle title={'Hello Worlds'}/>
-            Article 1
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Accordion titleValue={"Products"} collapsed={true}/>
-            <Accordion titleValue={"Services"} collapsed={false}/>
+        <div className="wrapper">
+            <header className="logo">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1667px-Apple_logo_black.svg.png" alt=""/>
+            </header>
+            <main className='cont'>
+                <aside className='nav'>
+                    <nav>
+                        <ul>
+                            <li>Profile</li>
+                            <li>Messages</li>
+                            <li>News</li>
+                            <li></li>
+                        </ul>
+                    </nav>
+                </aside>
+            </main>
+            <footer className='foot'></footer>
         </div>
     );
 }
-
-type PageTitlePropsType = {
-    title: String,
-}
-function PageTitle(props: PageTitlePropsType) {
-    console.log("AppTitle rendering")
-    return <h1>{props.title}</h1>;
-}
-
 export default App;
 
 
