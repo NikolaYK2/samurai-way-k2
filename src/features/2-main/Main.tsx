cce6c318bd5d553c7c5be454928602981ecd610b
k2 <izmail.k2@gmail.com>
2023-10-02 00:35:19 +0300

    add lazy

--- src/features/2-main/Main.tsx
+++ src/features/2-main/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import s from "./Main.module.css";
 import {Navigate, Route, Routes} from "react-router-dom";
 import {Nav} from "features/2-main/Navbar/Nav";
@@ -7,30 +7,35 @@ import {Music} from "features/2-main/content/music/Music";
 import {Settings} from "features/2-main/content/settings/Settings";
 import {FriendsContainer} from "features/2-main/content/friends/FriendsContainer";
 import {Login} from "features/0-auth/login/Login";
-import {DialogsMessage} from "features/2-main/content/dialogs/DialogsMessage";
-import ProfileContainer from "features/2-main/content/ContentProfileContainer";
 import UsersContainer from "features/2-main/content/users/UsersContiner";
+import {Loading} from "common/components/loading/Loading";
 
+const DialogsMessage = lazy(() => import('features/2-main/content/dialogs/DialogsMessage'));
+const ProfileContainer = lazy(() => import('features/2-main/content/ContentProfileContainer'));
 
 export const Main = () => {
-    return (
-        <main className={s.main}>
-            <Nav/>
-            <Routes>
-                <Route path="/samurai-way-k2" element={<Navigate to="/profile" replace/>}/>
-                <Route path='/profile' element={<ProfileContainer/>}>
-                    <Route path=":userId" element={<ProfileContainer/>}/>
-                </Route>
-                <Route path="/messages/*" element={<DialogsMessage/>}/>
-                <Route path="/users/*" element={<UsersContainer/>}/>
-                <Route path="/news" element={<News/>}/>
-                <Route path="/music" element={<Music/>}/>
-                <Route path="/settings" element={<Settings/>}/>
-                <Route path="/friends" element={<FriendsContainer/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="*" element={<div>404</div>}/>
-            </Routes>
-        </main>
-    );
-}
 
+
+  return (
+    <main className={s.main}>
+      <Nav/>
+      <Suspense fallback={<Loading/>}>
+        <Routes>
+          <Route path="/samurai-way-k2" element={<Navigate to="/profile" replace/>}/>
+          <Route path='/profile' element={<ProfileContainer/>}>
+            <Route path=":userId" element={<ProfileContainer/>}/>
+          </Route>
+          <Route path="/messages/*" element={<DialogsMessage/>}/>
+          <Route path="/users/*" element={<UsersContainer/>}/>
+          <Route path="/news" element={<News/>}/>
+          <Route path="/music" element={<Music/>}/>
+          <Route path="/settings" element={<Settings/>}/>
+          <Route path="/friends" element={<FriendsContainer/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="*" element={<div>404</div>}/>
+        </Routes>
+      </Suspense>
+
+    </main>
+  );
+}
--- src/features/2-main/content/dialogs/DialogsMessage.tsx
+++ src/features/2-main/content/dialogs/DialogsMessage.tsx
@@ -3,14 +3,14 @@ import s from "./DialogsMessage.module.css";
 import {Users} from "features/2-main/content/dialogs/users/Users";
 import MessageUsersContainer from "features/2-main/content/dialogs/messageUsers/MessageUsersContainer";
 
-type DialogsMessageType={
-}
-export const DialogsMessage: React.FC<DialogsMessageType> = (props ) => {
+type DialogsMessageType = {}
+const DialogsMessage: React.FC<DialogsMessageType> = () => {
 
-    return (
-        <div className={s.dialogs}>
-            <Users/>
-            <MessageUsersContainer/>
-        </div>
-    );
+  return (
+    <div className={s.dialogs}>
+      <Users/>
+      <MessageUsersContainer/>
+    </div>
+  );
 }
+export default DialogsMessage
