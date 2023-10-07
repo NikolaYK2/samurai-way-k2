5fe840e7d72f5fb5f6c5c61ba6913922fbdc5a52
k2 <izmail.k2@gmail.com>
2023-10-07 21:17:10 +0300

    refactor messagesUsers

--- src/features/2-main/content/dialogs/users/Users.tsx
+++ src/features/2-main/content/dialogs/users/Users.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import s from "./Users.module.css";
-import {store} from "app/redux-store";
+import {store, useAppSelector} from "app/redux-store";
 
 export const Users = () => {
-    return (
-        <div className={s.dialogs__users}>
-            Users
-            <>
-                {store.getState().messagesPage.users.map((u) => {
-                    return (
-                        <div key={u.id}>
-                            <NavLink to={u.link} className={({isActive}) => isActive ? s.active : s.users}>
-                                <div className={s.users__avatar}><img src={u.avatar} alt=""/></div>
-                                <span>{u.name}</span></NavLink>
-                        </div>
-                    )
-                })}
-            </>
-        </div>
-    )
+  const messages = useAppSelector(state => state.messagesPage.usersMessages)
+
+  return (
+    <div className={s.dialogs__users}>
+      {store.getState().messagesPage.users.map((u) => {
+        return (
+          <div key={u.id} className={s.users}>
+            <NavLink to={u.link} className={({isActive}) => isActive ? s.active : s.user}>
+              <div className={s.users__avatar}><img src={u.avatar} alt=""/></div>
+              <span>{u.name}</span>
+              <span>{}</span>
+            </NavLink>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
